f752fe8f0ef5f696602b91f038ef37b5ae1b0663
Julio Araujo <jcesarti2022@gmail.com>
2022-09-23 17:08:08 -0400

    Add files via upload

---
+++ desafio12.py
@@ -0,0 +1,6 @@
+''' escrever enunciado'''
+
+produto=float(input('Inserir o valor do produto:'))
+valor_dc=(produto*5)/100
+valor_final=produto-valor_dc
+print('O valor do produto com desconto e de:',valor_final)
