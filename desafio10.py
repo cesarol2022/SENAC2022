da25d39a52097d09241dcd2aa02efb910fc97247
Julio Araujo <jcesarti2022@gmail.com>
2022-09-23 16:51:24 -0400

    Add files via upload

---
+++ desafio10.py
@@ -0,0 +1,12 @@
+'''Crie um algoritmoque leia um numero real e mostre na tela o seu dobro e a
+sua terca parte.'''
+
+'''Ex:
+    Digite um numero: 3.5
+    O dobro de 3.5 e 7.0
+    A terca parte de 3.5 e 1.16666
+    '''
+larg=float(input('Largura da parede:' ))
+alt=float(input('Altura da parede: '))
+area = larg * alt
+print('Sua parede tem a dimensao de {} * {} e sua area e de {:.2f} m2.'.format)
