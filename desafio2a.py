7f62b277c0b408661ddc0f4c389cda23d46a39c1
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 09:46:36 -0400

    Add files via upload

--- desafio2a.py
+++ desafio2a.py
@@ -1,5 +1,12 @@
 '''
-Faca um programa que leia '''
+START DESAFIOS
+Desafio 02
+
+Crie um script Python que leia o dia o mês e o ano de
+nascimento de uma pessoa e mostre uma mensagem com a 
+data formatada.
+
+ '''
 dia= input('informe o dia do seu nascimento? ')
 mes= input('informe o mes do seunascimento? ')
 ano= input('informe o ano do seu nascimento? ')
--- desafio3a.py
+++ desafio3a.py
@@ -1,4 +1,12 @@
+'''
+START DESAFIOS
+Desafio 03
 
+Crie um script Python que leia dois números e tenta
+mostrar a soma entre eles.
+
+
+''' 
 a= float( input('qual o valor da variavel a?'))
 b= float( input('qual o valor da variavel b?'))
 soma= (a + b)
