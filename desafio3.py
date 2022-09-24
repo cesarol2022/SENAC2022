27cbcd0668de0203991c9c34a28678594615521d
cesarol2022 <jcesarti2022@gmail.com>
2022-09-24 07:17:59 -0400

    Create desafio3.py

---
+++ desafio3.py
@@ -0,0 +1,13 @@
+'''Crie um programa que leia o nome e o salário de um funcionário,
+mostrando no final uma mensagem
+
+
+Ex:
+Nome do funcionário: Maria do Carmo
+Salário: 1850,45
+O funcionário Maria do Carmo tem um salário de R$1850,45 em junho.
+'''
+nome=(input('Digito o nome do funcionário: '))
+salario=(input('Digite o salário: '))
+print('O funcionário',nome,'tem um salário de R$',salario,'em Junho.')
+
