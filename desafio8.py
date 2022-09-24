6f843fe0ca2d633ce4aefac5bd3b11c000c273b8
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 10:47:00 -0400

    Add files via upload

--- desafio8.py
+++ desafio8.py
@@ -1,13 +1,18 @@
-'''Crie um algoritmoque leia um numero real e mostre na tela o seu dobro e a
-sua terca parte.'''
+'''
+Desenvolva um programa que leia uma distancia em metros e mostre os 
+valores relativos em outras medidas.
 
-'''Ex:
-    Digite um numero: 3.5
-    O dobro de 3.5 e 7.0
-    A terca parte de 3.5 e 1.16666
-    '''
-numero= float(input('Informe o numero: '), )
-dobro= numero*2
-tercaparte= numero/3
-print (('O dobro desse numero e: '), dobro)
-print (('A terca parte deste numero e: '), tercaparte)
+Ex:
+Digite uma distância em metros: 185.72
+A distância de 85.7m corresponde a:
+
+0.18572Km                     1857.2dm
+1.18562Hm                     18572.0cm
+18.572Dam                     185720.mm
+
+'''
+numero= float(input('Informe a distância em metros: '), )
+print((numero/1000),('Km'),'                ',(numero*10),('dm'))
+print((numero/100),('Hm'),'                 ',(numero*100),('cm'))
+print((numero/10),('Dam'),'                ',(numero*1000),('mm'))
+
