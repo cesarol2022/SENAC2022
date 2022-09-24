12f986d13ac941df19c38f5481aa5b356bfb4886
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 11:06:56 -0400

    Add files via upload

--- desafio10.py
+++ desafio10.py
@@ -1,12 +1,12 @@
-'''Crie um algoritmoque leia um numero real e mostre na tela o seu dobro e a
-sua terca parte.'''
+'''
+Faça um algoritmo que leia a largura e altura de uma parede, calcule e 
+mostre a área a ser pintada e a quantidade de tinta necessáriapara o 
+serviço, sabendo que cada litro de tinta pinta uma área de 2 metros 
+quadrados.
 
-'''Ex:
-    Digite um numero: 3.5
-    O dobro de 3.5 e 7.0
-    A terca parte de 3.5 e 1.16666
-    '''
+'''
+
 larg=float(input('Largura da parede:' ))
-alt=float(input('Altura da parede: '))
+alt=float(input('Altura da parede:'))
 area = larg * alt
 print('Sua parede tem a dimensao de {} * {} e sua area e de {:.2f} m2.'.format)
