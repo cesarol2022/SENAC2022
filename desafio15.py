ae707772008d6e00bfe89bf1a43c2984525aea9f
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 16:25:59 -0400

    Add files via upload

--- desafio15.py
+++ desafio15.py
@@ -1,5 +1,5 @@
-'''Crie um programa que leia o numero de dias trabalhados em um mes
-e mostre o salario de um funcionario, sabendo que ele trabalha 8 horas
+'''Crie um programa que leia o número de dias trabalhados em um mês
+e mostre o salário de um funcionário, sabendo que ele trabalha 8 horas
 por dia e ganha R$25 por hora trabalhada.
 '''
 dias=float(input('Qual o numero de dias trabalhados? '))
