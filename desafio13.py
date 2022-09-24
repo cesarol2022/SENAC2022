d84f7a4d0002f4d4488b0c9395aada8f4708c9b4
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 16:24:05 -0400

    Add files via upload

--- desafio13.py
+++ desafio13.py
@@ -2,5 +2,5 @@
 e mostre o seu novo salario, com 15% de aumento.
 '''
 salario= float(input('Qual o salario do funcionario? R$' ))
-novo_salario=salario*1.15
+novo_salario=(salario+(salario*15/100))
 print(f'O novo salario com 15% de aumento e: R${novo_salario:,.2f}')
