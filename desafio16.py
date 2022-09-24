ec99e7dea3aa92be7f14eb4013aa6425a897d279
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 08:17:29 -0400

    Add files via upload

--- desafio16.py
+++ desafio16.py
@@ -7,6 +7,9 @@ ded vida a cada cigarro.
 Calcule quantos dias de vida um fumante perdera e exiba o total 
 em dias.
 '''
-cigarrosdia=float(('Qual a quantidade de cigarros fumados por dia? '))
-print(('O numero de dias a menos de vida e: '),(cigarrosdia) )
+cigarrosdia=float(input('Qual a quantidade de cigarros fumados por dia? '))
+anos_cigarro_fumou=float(input('Quantos anos ja fumou? '))
+menos_dia_vida=((cigarrosdia/144)*(anos_cigarro_fumou*365))
+print('O numero de dias a menos de vida e: ', menos_dia_vida)
+
 
