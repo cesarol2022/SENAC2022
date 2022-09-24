4a5a4e8c65d836856086d3f3738edd4dc8c19287
Julio Araujo <jcesarti2022@gmail.com>
2022-09-24 11:56:41 -0400

    Add files via upload

--- desafio12.py
+++ desafio12.py
@@ -1,6 +1,10 @@
-''' escrever enunciado'''
+'''
+Crie um programa que leia o preço de um produto, calcule e 
+mostre o seu PREÇO PROMOCIONAL, com 5% de desconto.
 
-produto=float(input('Inserir o valor do produto:'))
+'''
+
+produto=float(input('Inserir o valor do produto: '))
 valor_dc=(produto*5)/100
 valor_final=produto-valor_dc
 print('O valor do produto com desconto e de:',valor_final)
