ac666ca28eccb320c7b6697d3b5c036558564fa7
Julio Araujo <jcesarti2022@gmail.com>
2022-09-27 09:21:33 -0400

    Add files via upload

--- desafio8a.py
+++ desafio8a.py
@@ -13,9 +13,14 @@ Com o uso do comando format.
 
 '''
 numero= float(input('Informe a distância em metros: '), )
+n1=numero/1000
+n2=numero/100
+n3=numero/10
+n4=numero*10
+n5=numero*100
+n6=numero*1000
 
-print('A distancia {:.f} que corresponde a:',format(numero))
-print((numero/1000),('Km'),'                ',(numero*10),('dm'))
-print((numero/100),('Hm'),'                 ',(numero*100),('cm'))
-print((numero/10),('Dam'),'                ',(numero*1000),('mm'))
+print('A distância em quilomêtro:',format(n1),'Km','                ','A distância em decímetros:',format(n4),'dm')
+print('A distância em hectomêtros:',format(n2),'Hm','                ','A distância em centímetros:',format(n5),'cm')
+print('A distância em decimêtros:',format(n3),'Dam','              ','A distância em milímetros:',format(n6),('mm'))
 
