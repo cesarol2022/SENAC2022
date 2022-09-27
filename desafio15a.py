7aaa43e3079a3200c524a0e0f837e051b0ad7f95
Julio Araujo <jcesarti2022@gmail.com>
2022-09-27 08:01:59 -0400

    Add files via upload

--- desafio15a.py
+++ desafio15a.py
@@ -5,4 +5,4 @@ por dia e ganha R$25 por hora trabalhada.
 dias = float(input('Qual o numero de dias trabalhados? '))
 soma = dias * 8
 salario = soma * 25
-print('O salario e {}'.format(salario))
+print('O salario e {:.2f}'.format(salario))
