609acb30e5b47933f13824e6064ac5686b9e4146
Julio Araujo <jcesarti2022@gmail.com>
2022-09-27 07:39:55 -0400

    Add files via upload

---
+++ desafio16a.py
@@ -0,0 +1,22 @@
+'''
+Escreva um programa para calcular a reducao do tempo de vida de
+um fumante. Pergunte a quantidade de cigarros fumados por dias e
+quantos anos ele ja fumou. Considere que um fumante perde 10 min. 
+ded vida a cada cigarro.
+
+Calcule quantos dias de vida um fumante perdera e exiba o total 
+em dias.
+
+Ex: 6 cigarros por hora perde 1 hora de vida. Então 6 vezes 24 horas 
+fazem o total de 144 cigarros que faz o fumante perder 1 dia de vida.
+1 ano são 365 dias.
+'''
+cigarrosdia=float(input('Qual a quantidade de cigarros fumados por dia? '))
+anos_cigarro_fumou=float(input('Quantos anos ja fumou? '))
+menos_dia_vida=((cigarrosdia/144)*(anos_cigarro_fumou*365))
+
+# poderia ser feito assim na linha de baixo: print('O numero de dias a menos de vida e: ', menos_dia_vida)
+
+print('O número de dias a menos de vida é {:.2f}'.format(menos_dia_vida))
+
+
